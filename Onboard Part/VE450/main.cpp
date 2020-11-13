f52ff403457e06322b16914bd5faa75712e82c44
Yifeng <51085426+yifengit@users.noreply.github.com>
2020-11-13 02:34:31 +0000

    Update main.cpp

--- Onboard Part/VE450/main.cpp
+++ Onboard Part/VE450/main.cpp
@@ -8,26 +8,112 @@
 using namespace DJI::OSDK;
 using namespace DJI::OSDK::Telemetry;
 
+
 int main(int argc, char** argv) {
-  // Initialize variables
+  // Initialize server variables
+  int sock_fd;
+  struct sockaddr_in serv_addr;
+  int client_fd;
+  struct sockaddr_in client_add;
+  char buff[101];
+  socklen_t len;
+  int n;
+
+  // Initialize onboard variables
   int functionTimeout = 1;
 
   // Setup OSDK.
+  std::cout << "VE450 program starts!\n";
   LinuxSetup linuxEnvironment(argc, argv);
   Vehicle* vehicle = linuxEnvironment.getVehicle();
   if (vehicle == NULL) {
     std::cout << "Vehicle not initialized, exiting.\n";
     return -1;
   }
+  
 
   // Obtain Control Authority
   vehicle->obtainCtrlAuthority(functionTimeout);
+  
+  
+  // open server
+  // 创建socket连接
+  sock_fd=socket(AF_INET,SOCK_STREAM,0);
+  if(sock_fd==-1)
+  {
+      perror("create socket error!");
+      return 0;
+  }
+  else
+  {
+      printf("Success to create socket %d\n",sock_fd);
+  }
 
-  // Testing
-  std::cout
-          << "Automatic takeoff and landing test for VE450 starts..."
+  //设置server地址结构
+  bzero(&serv_addr,sizeof(serv_addr));    //初始化结构占用内存
+  serv_addr.sin_family=AF_INET;    //设置传输层类型IPv4
+  serv_addr.sin_port=htons(EHCO_PORT);    //设置端口号
+  serv_addr.sin_addr.s_addr=htons(INADDR_ANY);    //设置服务器IP地址
+  bzero(&(serv_addr.sin_zero),8);
+
+  //绑定端口
+  if(bind(sock_fd,(struct sockaddr*)&serv_addr,sizeof(serv_addr))!=0)
+  {
+       printf("bind address fail %d\n",errno);
+       close(sock_fd);
+       return 0;
+  }
+  else
+  {
+       printf("Success to bind address!\n");
+  }
+
+  //监听端口
+  if(listen(sock_fd,MAX_CLIENT_NUM!=0))
+  {
+      perror("listen socket error!\n");
+      close(sock_fd);
+      return 0;
+  }
+  else
+  {
+      printf("Success to listen\n");
+  }
+
+  //创建连接客户端的对应套接字
+  len=sizeof(client_add);
+  client_fd=accept(sock_fd,(struct sockaddr*)&client_add,&len);
+  if(client_fd<=0)
+  {
+      perror("accept error!");
+      close(sock_fd);
+      return 0;
+  }
+  //接收客户端发来的数据
+  while((n=recv(client_fd,buff,100,0))>0)
+  {
+      buff[n]=='\0';
+      
+      std::cout << "strcmp =" << strncmp(buff,"takeoff",7) << std::endl;
+      if (strncmp(buff,"takeoff",7)==0){
+        TakeoffAnyway(vehicle);
+        close(client_fd);
+        close(sock_fd);
+        return 0;
+      }
+      printf("receive byetes = %d data=%s\n",n,buff);
+      fflush(stdout);
+      send(client_fd,buff,n,0);
+      if(strncmp(buff,"quit",4)==0)
+      {
+          break;
+      }
+  }
+    
+
+  std::cout << "Automatic takeoff and landing test for VE450 starts..."
           <<std::endl;
-  TakeoffAnyway(vehicle);
+  /*TakeoffAnyway(vehicle);
   moveByPositionOffset(vehicle,0,0,0,0);
   moveByPositionOffset(vehicle,0,0,0,0);
   moveByPositionOffset(vehicle,0,0,0,0);
@@ -67,5 +153,8 @@ int main(int argc, char** argv) {
       break;
   }*/
 
+  // close server
+  close(client_fd);
+  close(sock_fd);
   return 0;
-}
+} 
